6895bb716317ffb2bfdf7757fe8a658cee7d27c0
ajaymeena9069 <amm62408@gmail.com>
2025-12-19 22:22:37 +0530

    Algo: Find missing number in 1 to n

---
+++ MissingNumber.java
@@ -0,0 +1,19 @@
+public class MissingNumber {
+
+    public static int findMissing(int arr[], int n) {
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            actualSum = actualSum + arr[i];
+        }
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 6, 7 };
+        int n = 7;
+        int missingNumer = findMissing(arr, n);
+        System.out.println("Missing Number is -> " + missingNumer);
+    }
+}
